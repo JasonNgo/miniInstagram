a0c9d07dd0c1fd0d250c79d481cb56bb68175aba
Jason Ngo <jasonnngo@gmail.com>
2018-08-19 15:39:16 -0400

    Initial Commit

---
+++ LBTA-Instagram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LBTA-Instagram
+//
+//  Created by Jason Ngo on 2018-08-19.
+//  Copyright © 2018 Jason Ngo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
