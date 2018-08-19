67d62f768e73c5b6811d41deb7647d7f72d406a2
Jason Ngo <jasonnngo@gmail.com>
2018-08-19 16:27:31 -0400

    Setup sign up page views and added project assets.

--- LBTA-Instagram/ViewController.swift
+++ LBTA-Instagram/ViewController.swift
@@ -9,17 +9,92 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: Views
+    let addPhotoButton: UIButton = {
+        var button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "plus_photo"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let emailTextField: UITextField = {
+        var tf = UITextField()
+        tf.placeholder = "Email"
+        tf.borderStyle = .roundedRect
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        tf.font = UIFont.systemFont(ofSize: 14)
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        return tf
+    }()
+    
+    let usernameTextField: UITextField = {
+        var tf = UITextField()
+        tf.placeholder = "Username"
+        tf.borderStyle = .roundedRect
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        tf.font = UIFont.systemFont(ofSize: 14)
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        return tf
+    }()
+    
+    let passwordTextField: UITextField = {
+        var tf = UITextField()
+        tf.placeholder = "Password"
+        tf.isSecureTextEntry = true
+        tf.borderStyle = .roundedRect
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        tf.font = UIFont.systemFont(ofSize: 14)
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        return tf
+    }()
+    
+    let signUpButton: UIButton = {
+        var button = UIButton(type: .system)
+        button.setTitle("Sign Up", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.backgroundColor = UIColor(red: 149/255, green: 204/255, blue: 244/255, alpha: 1)
+        button.layer.cornerRadius = 5
+        return button
+    }()
+    
+    // MARK: - Lifecycle Functions
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setupViews()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
+    // MARK: - Setup Functions
+    
+    fileprivate func setupViews() {
+        view.addSubview(addPhotoButton)
+        setupAddPhotoButton()
+        setupInputFields()
+    } // setupViews
+    
+    fileprivate func setupAddPhotoButton() {
+        addPhotoButton.heightAnchor.constraint(equalToConstant: 140).isActive = true
+        addPhotoButton.widthAnchor.constraint(equalToConstant: 140).isActive = true
+        addPhotoButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        addPhotoButton.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 40).isActive = true
+    } // setupAddPhotoButton
+    
+    fileprivate func setupInputFields() {
+        let stackView = UIStackView(arrangedSubviews: [emailTextField, usernameTextField, passwordTextField, signUpButton])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.spacing = 10
+        
+        view.addSubview(stackView)
+        stackView.topAnchor.constraint(equalTo: addPhotoButton.bottomAnchor, constant: 20).isActive = true
+        stackView.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 40).isActive = true
+        stackView.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -40).isActive = true
+        stackView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+    } // setupInputFields
+    
 }
 
