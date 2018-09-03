e32893821ea88356ef5a888a7bea995f4b2d73d5
Jason Ngo <jason.n.ngo@gmail.com>
2018-09-02 20:40:01 -0400

    - added custom animations when displaying the camera using the bar button - added a container view for the preview image view

--- LBTA-Instagram/Main/HomeFeed/CameraController.swift
+++ LBTA-Instagram/Main/HomeFeed/CameraController.swift
@@ -9,9 +9,7 @@
 import UIKit
 import AVFoundation
 
-class CameraController: UIViewController, AVCapturePhotoCaptureDelegate {
-    
-    let output = AVCapturePhotoOutput()
+class CameraController: UIViewController {
     
     // MARK: - Views and Selectors
     
@@ -42,58 +40,29 @@ class CameraController: UIViewController, AVCapturePhotoCaptureDelegate {
         output.capturePhoto(with: settings, delegate: self)
     }
     
-    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
-        guard error == nil else {
-            print("failed to capture photo: \(String(describing: error))")
-            return
-        }
-        
-        guard let imageData = photo.fileDataRepresentation() else { return }
-        guard let image = UIImage.init(data: imageData, scale: 1.0) else { return }
-        
-        let previewImage = UIImageView(image: image)
-        view.addSubview(previewImage)
-        previewImage.anchor(top: view.topAnchor, paddingTop: 0, right: view.rightAnchor, paddingRight: 0,
-                            bottom: view.bottomAnchor, paddingBottom: 0, left: view.leftAnchor, paddingLeft: 0,
-                            width: 0, height: 0)
+    // MARK: - Properties
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
     }
     
-    func photoOutput(_ captureOutput: AVCapturePhotoOutput, didFinishProcessingPhoto photoSampleBuffer: CMSampleBuffer?, previewPhoto previewPhotoSampleBuffer: CMSampleBuffer?, resolvedSettings: AVCaptureResolvedPhotoSettings, bracketSettings: AVCaptureBracketedStillImageSettings?, error: Error?) {
-        
-    }
+    let output = AVCapturePhotoOutput()
+    let customAnimationPresentor = CustomAnimationPresentor()
+    let customAnimationDismisser = CustomAnimationDismisser()
     
-    
-    let cancelPhotoButton: UIButton = {
-        var button = UIButton(type: .system)
-        button.setImage(#imageLiteral(resourceName: "cancel_shadow").withRenderingMode(.alwaysOriginal), for: .normal)
-        button.addTarget(self, action: #selector(handleCancelPhoto), for: .touchUpInside)
-        return button
-    }()
-    
-    @objc func handleCancelPhoto() {
-        print("cancel photo pressed")
-    }
+    // MARK: - Lifecycle functions
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // UIViewControllerTransitioningDelegate
+        transitioningDelegate = self
         
         setupCaptureSession()
         setupHUD()
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        // Sets the status bar to hidden when the view has finished appearing
-        if let statusBar: UIView = UIApplication.shared.value(forKey: "statusBar") as? UIView {
-            statusBar.isHidden = true
-        }
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        // Sets the status bar to visible when the view is about to disappear
-        if let statusBar: UIView = UIApplication.shared.value(forKey: "statusBar") as? UIView {
-            statusBar.isHidden = false
-        }
-    }
+    // MARK: - Set Up Functions
     
     fileprivate func setupCaptureSession() {
         let captureSession = AVCaptureSession()
@@ -134,11 +103,40 @@ class CameraController: UIViewController, AVCapturePhotoCaptureDelegate {
         dismissButton.anchor(top: view.topAnchor, paddingTop: 12, right: view.rightAnchor, paddingRight: -12,
                              bottom: nil, paddingBottom: 0, left: nil, paddingLeft: 0,
                              width: 50, height: 50)
-        
-        view.addSubview(cancelPhotoButton)
-        cancelPhotoButton.anchor(top: view.topAnchor, paddingTop: 12, right: nil, paddingRight: 0,
-                                 bottom: nil, paddingBottom: 0, left: view.leftAnchor, paddingLeft: 12,
-                                 width: 50, height: 50)
     }
-
+    
 } // CameraController
+
+// MARK: - AVCaptureDelegate
+extension CameraController: AVCapturePhotoCaptureDelegate {
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        guard error == nil else {
+            print("failed to capture photo: \(String(describing: error))")
+            return
+        }
+        
+        guard let imageData = photo.fileDataRepresentation() else { return }
+        guard let previewImage = UIImage.init(data: imageData, scale: 1.0) else { return }
+        
+        let containerView = PreviewImageContainerView()
+        containerView.previewImage = previewImage
+        
+        view.addSubview(containerView)
+        containerView.anchor(top: view.topAnchor, paddingTop: 0, right: view.rightAnchor, paddingRight: 0,
+                             bottom: view.bottomAnchor, paddingBottom: 0, left: view.leftAnchor, paddingLeft: 0,
+                             width: 0, height: 0)
+    }
+}
+
+// MARK: - UIViewControllerTransitioningDelegate
+extension CameraController: UIViewControllerTransitioningDelegate {
+    
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return customAnimationPresentor
+    }
+    
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return customAnimationDismisser
+    }
+    
+}
---
+++ LBTA-Instagram/Main/HomeFeed/CustomAnimationTransitions.swift
@@ -0,0 +1,56 @@
+//
+//  CustomAnimationTransitions.swift
+//  LBTA-Instagram
+//
+//  Created by Jason Ngo on 2018-09-02.
+//  Copyright © 2018 Jason Ngo. All rights reserved.
+//
+
+import UIKit
+
+class CustomAnimationPresentor: NSObject, UIViewControllerAnimatedTransitioning {
+    
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 0.5
+    }
+    
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        let containerView = transitionContext.containerView
+        guard let fromContainerView = transitionContext.view(forKey: .from) else { return }
+        guard let toContainerView = transitionContext.view(forKey: .to) else { return }
+        containerView.addSubview(toContainerView)
+        
+        let startingFrame = CGRect(x: -toContainerView.frame.width, y: 0, width: toContainerView.frame.width, height: toContainerView.frame.height)
+        toContainerView.frame = startingFrame
+        
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            fromContainerView.frame = CGRect(x: fromContainerView.frame.width, y: 0, width: containerView.frame.width, height: containerView.frame.height)
+            toContainerView.frame = CGRect(x: 0, y: 0, width: toContainerView.frame.width, height: toContainerView.frame.height)
+        }) { (_) in
+            transitionContext.completeTransition(true)
+        }
+    }
+    
+} // CustomAnimationPresentor
+
+class CustomAnimationDismisser: NSObject, UIViewControllerAnimatedTransitioning {
+    
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 0.5
+    }
+    
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        let containerView = transitionContext.containerView
+        guard let fromContainerView = transitionContext.view(forKey: .from) else { return }
+        guard let toContainerView = transitionContext.view(forKey: .to) else { return }
+        containerView.addSubview(toContainerView)
+        
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            fromContainerView.frame = CGRect(x: -fromContainerView.frame.width, y: 0, width: containerView.frame.width, height: containerView.frame.height)
+            toContainerView.frame = CGRect(x: 0, y: 0, width: toContainerView.frame.width, height: toContainerView.frame.height)
+        }) { (_) in
+            transitionContext.completeTransition(true)
+        }
+    }
+    
+} // CustomAnimationDismisser
