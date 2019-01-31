f0025deb8574d2bb99ba5530a158cd81302b7960
Jason Ngo <jason.n.ngo@gmail.com>
2019-01-30 23:57:11 -0500

    - Added Coordinator protocol

---
+++ LBTA-Instagram/Util/Coordinator.swift
@@ -0,0 +1,13 @@
+//
+//  Coordinator.swift
+//  LBTA-Instagram
+//
+//  Created by Jason Ngo on 2019-01-30.
+//  Copyright © 2019 Jason Ngo. All rights reserved.
+//
+
+import Foundation
+
+protocol Coordinator {
+    func start()
+}
